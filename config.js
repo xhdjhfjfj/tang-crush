6f3e9b0a356af8b49304cda391c2f337d4f77c1d
xhdjhfjfj <87372971+xhdjhfjfj@users.noreply.github.com>
2021-07-13 19:34:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,14 @@ const CONFIG = {
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
+    title: 'Phải chăng bấy bì đã nhớ anh này ùi 😙',
+    desc: 'Phải chăng em đã muốn gặp anh này đây😘 ',
+    btnYes: ' Nhớ tới công chiện nuôn <33',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    question:'mà sao trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Đợi hết dịch anh chở đi nhậu hen bấy bi à:>>>.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
-}
